0aedbb21c0a5a6614838624db0310618b59ec03d
mrkirby153 <mr.austinwhyte@gmail.com>
2017-07-11 15:29:05 -0700

    Publish all messages received in the guild

--- discord/src/main/kotlin/me/mrkirby153/bridgebot/discord/BridgeBot.kt
+++ discord/src/main/kotlin/me/mrkirby153/bridgebot/discord/BridgeBot.kt
@@ -4,14 +4,17 @@ import me.mrkirby153.bridgebot.discord.redis.RedisConnector
 import net.dv8tion.jda.core.AccountType
 import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.JDABuilder
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import net.dv8tion.jda.core.hooks.ListenerAdapter
 import java.util.*
 
-class BridgeBot(val properties: Properties) {
-
+class BridgeBot(val properties: Properties) : ListenerAdapter() {
 
     val jda: JDA
 
     val redis: RedisConnector
+
+    val owner = properties.getProperty("owner_id") ?: ""
 
     init {
         jda = buildJDA(properties.getProperty("api_token"))
@@ -21,5 +24,17 @@ class BridgeBot(val properties: Properties) {
 
     private fun buildJDA(token: String) = JDABuilder(AccountType.BOT).apply {
         setToken(token)
+        addEventListener(this@BridgeBot)
     }.buildBlocking()
+
+    override fun onMessageReceived(event: MessageReceivedEvent?) {
+        if (event == null)
+            return
+        if(event.message.rawContent == "%%shutdown" && event.author.id == this.owner) {
+            event.message.channel.sendMessage("Shutting down... :wave:").complete()
+            jda.shutdown()
+            return
+        }
+        redis.publish(event.message)
+    }
 }
