13e648ac3641069dd311fc27a93f7b6f6bcbe49c
mrkirby153 <mr.austinwhyte@gmail.com>
2017-07-14 11:50:32 -0700

    Strip color codes from chat and death messages

--- spigot/src/main/kotlin/me/mrkirby153/bridgebot/spigot/chat/ChatListener.kt
+++ spigot/src/main/kotlin/me/mrkirby153/bridgebot/spigot/chat/ChatListener.kt
@@ -1,6 +1,7 @@
 package me.mrkirby153.bridgebot.spigot.chat
 
 import me.mrkirby153.bridgebot.spigot.Bridge
+import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -13,30 +14,7 @@ class ChatListener(val plugin: Bridge) : Listener {
     fun onChat(event: AsyncPlayerChatEvent) {
         ChatHandler.getChannels().forEach { chan ->
             if (chan.twoWay) {
-                plugin.redisConnector.sendChatMessage(chan.server, chan.channel, event.player, event.message)
-                /*val builder = MultipartBody.Builder().apply {
-                    setType(MultipartBody.FORM)
-                    addFormDataPart("content", event.message)
-                    addFormDataPart("username", event.player.name)
-                    addFormDataPart("avatar_url", "https://minotar.net/helm/${event.player.name}")
-                }
-
-                val request = Request.Builder().apply {
-                    url(chan.webhook)
-                    method("POST", RequestBody.create(null, ByteArray(0)))
-                    post(builder.build())
-                }.build()
-                plugin.httpClient.newCall(request).enqueue(object : Callback {
-                    override fun onResponse(call: Call?, response: Response?) {
-
-                    }
-
-                    override fun onFailure(call: Call?, e: IOException?) {
-                        e?.printStackTrace()
-                        plugin.logger.severe("Webhook ${chan.webhook} failed to execute!")
-                    }
-
-                })*/
+                plugin.redisConnector.sendChatMessage(chan.server, chan.channel, event.player, ChatColor.stripColor(event.message))
             }
         }
     }
@@ -45,7 +23,7 @@ class ChatListener(val plugin: Bridge) : Listener {
     fun onDeath(event: PlayerDeathEvent) {
         ChatHandler.getChannels().forEach { chan ->
             if (chan.twoWay) {
-                plugin.redisConnector.sendChatMessage(chan.server, chan.channel, null, event.deathMessage)
+                plugin.redisConnector.sendChatMessage(chan.server, chan.channel, null, ChatColor.stripColor(event.deathMessage))
             }
         }
     }
